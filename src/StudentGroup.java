0e6cb8771e3b07e68b6c05282cb927a0683d7240
dandamudi85 <dandamudi85@gmail.com>
2017-09-14 23:34:14 +0530

    Add files via upload
    sucees

---
+++ src/StudentGroup.java
@@ -0,0 +1,173 @@
+import java.util.Date;
+
+/**
+ * A fix-sized array of students
+ * array length should always be equal to the number of stored elements
+ * after the element was removed the size of the array should be equal to the number of stored elements
+ * after the element was added the size of the array should be equal to the number of stored elements
+ * null elements are not allowed to be stored in the array
+ * 
+ * You may add new methods, fields to this class, but DO NOT RENAME any given class, interface or method
+ * DO NOT PUT any classes into packages
+ *
+ */
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+	
+	/**
+	 * DO NOT remove or change this constructor, it will be used during task check
+	 * @param length
+	 */
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+	}
+
+	@Override
+	public Student[] getStudents() {
+		// Add your implementation here
+		return students;
+	}
+
+	@Override
+	public void setStudents(Student[] students) {
+		// Add your implementation here
+		if(students == NULL)
+		{
+			throw new IllegalArgumentException("student is null");
+		}
+		else
+		{
+			Student[] stud = getStudents();
+			for(int i=0;i<students.length;i++)
+			{
+				stud[i] = students[i]; 
+			}
+		}
+	}
+
+	@Override
+	public Student getStudent(int index) {
+		// Add your implementation here
+		if((index <= 0) || (index >= students.length))
+		{
+			throw new IllegalArgumentException("index out of range");
+		}
+		else
+		{
+			Students s1 = getStudents();
+			Student s = s1[index];
+		return s;
+		}
+	}
+
+	@Override
+	public void setStudent(Student student, int index) {
+		// Add your implementation here
+		if(student  == null)
+		{
+			throw new IllegalArgumentException("student is null inside setStudent");
+		}
+		Student[] stu = getStudents();
+		else if((index <= 0 || index >= stu.length)
+				{
+					throw new IllegalArgumentException("index out of rangeinside setStudent");
+				}
+		else
+		{
+			stu[index] = student;
+		}
+	}
+
+	@Override
+	public void addFirst(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void addLast(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void add(Student student, int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void remove(int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void remove(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeFromIndex(int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeFromElement(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeToIndex(int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeToElement(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void bubbleSort() {
+		// Add your implementation here
+	}
+
+	@Override
+	public Student[] getByBirthDate(Date date) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getNearBirthDate(Date date, int days) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public int getCurrentAgeByDate(int indexOfStudent) {
+		// Add your implementation here
+		return 0;
+	}
+
+	@Override
+	public Student[] getStudentsByAge(int age) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getStudentsWithMaxAvgMark() {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student getNextStudent(Student student) {
+		// Add your implementation here
+		return null;
+	}
+}
